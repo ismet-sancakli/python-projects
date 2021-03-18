2a86bb600e4c37bb288fbf4bc0912485f3ca5442
İsmet Sancaklı <isancakli5@gmail.com>
2021-03-18 15:31:56 +0300

    Snake Game

---
+++ Snake_Game/main.py
@@ -0,0 +1,48 @@
+from turtle import Turtle, Screen
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+import time
+
+WIDHT = 600
+HEIGHT = 600
+screen = Screen()
+screen.setup(WIDHT, HEIGHT)
+screen.bgcolor("black")
+screen.title("My snake game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.06)
+    snake.move()
+
+    if snake.head.distance(food) < 17:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    # Detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+    # Detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+
+
+screen.exitonclick()
---
+++ Snake_Game/scoreboard.py
@@ -0,0 +1,30 @@
+from turtle import Turtle
+
+WIDHT = 600
+HEIGHT = 600
+ALIGNMENT = "center"
+FONT = ("Arial", 24, "normal")
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0, (HEIGHT/2)-40)
+        self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.write(f"Score : {self.score}", align="center", font=("Arial", 24, "normal"))
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER.", align="center", font=("Arial", 24, "normal"))
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
